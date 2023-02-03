f4025a3b34e8f02e7a7825f7a2b8f4a94c37a83d
Alvaro Fernandez <alvaro.fdezalvarez@gmail.com>
2023-02-03 18:25:50 +0100

    vue router exercise second try

---
+++ labs/class-challenges/vue-router-second-try/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref, reactive } from "vue";
+const homePage = "Welcome to the Home page";
+</script>
+
+<template>
+  <main>
+    <h1>{{ homePage }}</h1>
+  </main>
+</template>
